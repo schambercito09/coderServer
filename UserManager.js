1dcd24e9bb840a738fdb0c055cc5275ec60258e1
Schambercito <schamberger.juanignacio@gmail.com>
2024-03-11 16:45:40 -0300

    users

---
+++ UserManager.js
@@ -0,0 +1,32 @@
+class UserManager {
+  static #users = [];
+
+  create(data) {
+    const user = {
+      id:
+        UserManager.#users.length === 0
+          ? 1
+          : UserManager.#users[UserManager.#users.length - 1].id + 1,
+      photo: data.photo,
+      email: data.email,
+      password: data.password,
+      role: 0,
+    };
+    UserManager.#users.push(user);
+    console.log("usuarioCreado");
+  }
+  read(){
+    return UserManager.#users
+  }
+}
+
+const usersManager = new UserManager()
+usersManager.create({
+    photo: "photo.png",
+    email:"coder@gmail.com",
+    password:"hola1234",
+})
+//crear un usurio mas
+
+console.log(usersManager.read())
+
